f03a39534c537d8eba7683b2f7623b4451dec3d6
Samuel Maia <samuelmvf@gmail.com>
2025-02-18 23:52:25 -0300

    feat: add "ApplicationLogo" component

---
+++ components/application/logo/application-logo.tsx
@@ -0,0 +1,12 @@
+import { CircleDollarSign } from "lucide-react";
+
+export function ApplicationLogo() {
+  return (<div className={"flex gap-3 justify-center items-center select-none"}>
+    <CircleDollarSign size={40} className={"text-yellow-500"} />
+
+    <h1 className={"text-2xl"}>
+      <p className={"text-gray-500 dark:text-white"}>Smart</p>
+      <p className={"text-yellow-500 -mt-3 ml-3"}>Hourly.</p>
+    </h1>
+  </div>);
+}
