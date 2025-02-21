55ec8d91ad99bb79a6e1acfd48bef0af6e1f83fa
Samuel Maia <samuelmvf@gmail.com>
2025-02-21 02:24:41 -0300

    style: change the margins/paddings of the root layout

--- components/application/content/application-content.tsx
+++ components/application/content/application-content.tsx
@@ -2,9 +2,10 @@ import ApplicationSidebar from "@/components/application/sidebar/application-sid
 
 export function ApplicationContent ({ children } : Readonly<{ children: React.ReactNode }>) {
   return <section className={"bg-gray-200 dark:bg-gray-900"} style={{height: "calc(100vh - 5rem)"}}>
-    <div className={"container w-full h-full max-w-6xl flex py-4"}>
+    <div className={"container w-full h-full max-w-6xl flex py-6"}>
       <ApplicationSidebar />
-      <div className={"flex-1 mx-4"}>
+
+      <div className={"flex-1 mx-6"}>
         {children}
       </div>
     </div>
--- components/application/sidebar/application-sidebar.tsx
+++ components/application/sidebar/application-sidebar.tsx
@@ -1,37 +1,42 @@
+'use client'
+
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { LucideCog, LucideFileStack, LucideLayoutDashboard } from "lucide-react";
 import { SidebarItem, SidebarItemProps } from "@/components/application/sidebar/item/sidebar-item";
+import { usePathname } from "next/navigation";
 
 const menuItems: SidebarItemProps[] = [
   {
     title: "Dashboard",
-    link: "/dashboard",
-    description: "See details of your orders",
+    link: "/",
+    description: "Overview of your business",
     Icon: LucideLayoutDashboard,
   },
   {
     title: "Orders",
     link: "/orders",
-    description: "Manage your orders",
+    description: "View and manage customer orders",
     Icon: LucideFileStack
   },
   {
     title: "Settings",
     link: "/settings",
+    description: "Configure your costs",
     Icon: LucideCog,
     showSeparator: false
   }
-]
+];
 
 export default function ApplicationSidebar () {
+  const pathName = usePathname()
+
   return (
     <Card className={"bg-gray-100 dark:bg-gray-800 min-w-60 max-w-64 h-full flex flex-col justify-between"}>
-      <CardContent className={"flex-1 px-0 mt-4"}>
-        {menuItems.map(item => {
-          return <SidebarItem {...item}/>
-        })}
+      <CardContent className={"flex-1 px-0"}>
+        { menuItems.map(item => <SidebarItem {...item} key={item.link} active={item.link === pathName}/>) }
       </CardContent>
-      <CardFooter className={"flex flex-col p-8 items-center text-gray-300 dark:text-white select-none"}>
+
+      <CardFooter className={"flex flex-col p-6 items-center text-gray-300 dark:text-white select-none"}>
         <span>© 2021 SmartHourly</span>
         <span>Version 0.0.1</span>
       </CardFooter>
