15106b6d0559164d6d14c0884712fdd31fd419fc
Samuel Maia <samuelmvf@gmail.com>
2025-02-24 23:15:39 -0300

    chore: add clerk.js for authentication, add src folder and update the app config to point to src

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,12 @@
 import type { Config } from "tailwindcss";
+import tailwindcss_animate from "tailwindcss-animate";
 
 export default {
     darkMode: ["class"],
     content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
 	theme: {
     	container: {
@@ -68,8 +69,17 @@ export default {
     			lg: 'var(--radius)',
     			md: 'calc(var(--radius) - 2px)',
     			sm: 'calc(var(--radius) - 4px)'
-    		}
+    		},
+				keyframes: {
+					'caret-blink': {
+						'0%,70%,100%': { opacity: '1' },
+						'20%,50%': { opacity: '0' },
+					},
+				},
+				animation: {
+					'caret-blink': 'caret-blink 1.25s ease-out infinite',
+				},
     	}
     },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [ tailwindcss_animate ],
 } satisfies Config;
