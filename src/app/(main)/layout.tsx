e30d33c6ae7703dd500960ad188607bd99e7b33c
Samuel Maia <samuelmvf@gmail.com>
2025-02-25 22:45:10 -0300

    refactor: sign-in page and create a new base layout for auth routes

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from "next";
 import "../globals.css";
 
-import { ApplicationHeader } from "@/components/application/header/application-header";
 import { ApplicationContent } from "@/components/application/content/application-content";
-
+import { ApplicationHeader } from "@/components/application/header/application-header";
 
 export const metadata: Metadata = {
   title: "Dashboard | SmartHourly",
@@ -12,14 +11,14 @@ export const metadata: Metadata = {
   robots: "noindex, nofollow",
 };
 
-
-export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return (<>
+export default function MainLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <>
       <ApplicationHeader />
 
-      <ApplicationContent>
-        {children}
-      </ApplicationContent>
+      <ApplicationContent>{children}</ApplicationContent>
     </>
   );
 }
