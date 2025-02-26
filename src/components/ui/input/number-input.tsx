1e6149811f24a88178c29d24cab568ce8d58ed32
Samuel Maia <samuelmvf@gmail.com>
2025-02-26 16:17:13 -0300

    feat: add "NumberInput" component

---
+++ src/components/ui/input/number-input.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import { useReducer } from "react";
+import { UseFormReturn } from "react-hook-form";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../form";
+import { Input } from "./input";
+
+type NumberInputProps = {
+  form: UseFormReturn<any>;
+  name: string;
+  label: string;
+  placeholder: string;
+  disabled?: boolean;
+};
+
+const numericCharactersRegex = /\D/g;
+
+export const NumberInput = function ({
+  form,
+  name,
+  label,
+  placeholder,
+  disabled = false,
+}: NumberInputProps) {
+  const initialValue = form.getValues()[name] || 0;
+
+  const [value, setValue] = useReducer((_: any, next: string) => {
+    return Number(next.replace(numericCharactersRegex, ""));
+  }, initialValue);
+
+  function handleChange(realChangeFn: Function, formattedValue: string) {
+    const digits = formattedValue.replace(numericCharactersRegex, "");
+    realChangeFn(Number(digits));
+  }
+
+  return (
+    <FormField
+      control={form.control}
+      name={name}
+      render={({ field }) => {
+        field.value = value;
+        const _change = field.onChange;
+
+        return (
+          <FormItem>
+            <FormLabel>{label}</FormLabel>
+            <FormControl>
+              <Input
+                placeholder={placeholder}
+                type='text'
+                {...field}
+                disabled={disabled}
+                onChange={(ev) => {
+                  setValue(ev.target.value);
+                  handleChange(_change, ev.target.value);
+                }}
+                value={value}
+              />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        );
+      }}
+    />
+  );
+};
