0b6012f0df0270545e4c12501400fb1929cd2424
Samuel Maia <samuelmvf@gmail.com>
2025-02-27 20:33:33 -0300

    style: improve general layout style

--- src/components/application/content/application-content.tsx
+++ src/components/application/content/application-content.tsx
@@ -1,13 +1,15 @@
 import ApplicationSidebar from "@/components/application/sidebar/application-sidebar";
 
-export function ApplicationContent ({ children } : Readonly<{ children: React.ReactNode }>) {
-  return <section className={"bg-gray-200 dark:bg-gray-900"} style={{height: "calc(100vh - 5rem)"}}>
-    <div className={"container w-full h-full max-w-6xl flex py-6"}>
-      <ApplicationSidebar />
+export function ApplicationContent({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <section className={"bg-gray-200 dark:bg-gray-900 h-[calc(100vh-5rem)]"}>
+      <div className={"container w-full h-full max-w-6xl flex px-6 py-6"}>
+        <ApplicationSidebar />
 
-      <div className={"flex-1 mx-6"}>
-        {children}
+        <div className={"flex-1 ml-6"}>{children}</div>
       </div>
-    </div>
-  </section>
+    </section>
+  );
 }
--- src/components/application/sidebar/application-sidebar.tsx
+++ src/components/application/sidebar/application-sidebar.tsx
@@ -1,9 +1,19 @@
-'use client'
+"use client";
+
+import { usePathname } from "next/navigation";
+
+import {
+  SidebarItem,
+  SidebarItemProps,
+} from "@/components/application/sidebar/item/sidebar-item";
 
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
-import { LucideCog, LucideFileStack, LucideLayoutDashboard } from "lucide-react";
-import { SidebarItem, SidebarItemProps } from "@/components/application/sidebar/item/sidebar-item";
-import { usePathname } from "next/navigation";
+
+import {
+  LucideCog,
+  LucideFileStack,
+  LucideLayoutDashboard,
+} from "lucide-react";
 
 const menuItems: SidebarItemProps[] = [
   {
@@ -16,27 +26,41 @@ const menuItems: SidebarItemProps[] = [
     title: "Orders",
     link: "/orders",
     description: "View and manage customer orders",
-    Icon: LucideFileStack
+    Icon: LucideFileStack,
   },
   {
     title: "Settings",
     link: "/settings",
     description: "Configure your costs",
     Icon: LucideCog,
-    showSeparator: false
-  }
+    showSeparator: false,
+  },
 ];
 
-export default function ApplicationSidebar () {
-  const pathName = usePathname()
+export default function ApplicationSidebar() {
+  const pathName = usePathname();
 
   return (
-    <Card className={"bg-gray-100 dark:bg-gray-800 min-w-60 max-w-64 h-full flex flex-col justify-between"}>
+    <Card
+      className={
+        "bg-gray-100 dark:bg-gray-800 min-w-64 w-72 h-full flex flex-col justify-between"
+      }
+    >
       <CardContent className={"flex-1 px-0"}>
-        { menuItems.map(item => <SidebarItem {...item} key={item.link} active={item.link === pathName}/>) }
+        {menuItems.map((item) => (
+          <SidebarItem
+            {...item}
+            key={item.link}
+            active={item.link === pathName}
+          />
+        ))}
       </CardContent>
 
-      <CardFooter className={"flex flex-col p-6 items-center text-gray-300 dark:text-white select-none"}>
+      <CardFooter
+        className={
+          "flex flex-col p-6 items-center text-gray-300 dark:text-white select-none"
+        }
+      >
         <span>© 2021 SmartHourly</span>
         <span>Version 0.0.1</span>
       </CardFooter>
