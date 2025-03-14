2572a4972b841aefafc4e10335d35facd2a9e289
Samuel Maia <samuelmvf@gmail.com>
2025-03-14 17:26:22 -0300

    feat: use new Providers component on main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
+import { Providers } from "@/components/providers/providers";
+import { Icons } from "@/components/ui/icons";
+import { Toaster } from "@/components/ui/sonner";
+import { ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ClerkLoaded, ClerkLoading, ClerkProvider } from "@clerk/nextjs";
-import { Icons } from "@/components/ui/icons";
-import { ProtectedRoutes, PublicRoutes } from "@/lib/routes";
 
 export const metadata: Metadata = {
   title: "Sign In | SmartHourly",
@@ -16,28 +17,20 @@ const interFont = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <ClerkProvider
-      signInForceRedirectUrl={ProtectedRoutes.Dashboard}
-      afterSignOutUrl={PublicRoutes.SignIn}
-      signInUrl={PublicRoutes.SignIn}
-      signUpUrl={PublicRoutes.SignUp}
-    >
-      <html lang="en">
-        <body
-          className={`${interFont.className} antialiased bg-gray-100 dark:bg-gray-800 text-primary dark:text-white box-border`}
-        >
+    <Providers>
+      <html lang='en'>
+        <body className={`${interFont.className} antialiased bg-gray-100 dark:bg-gray-800 text-primary dark:text-white box-border`}>
           <ClerkLoading>
             <div className={"w-full h-[100vh] flex justify-center items-center gap-3"}>
-              <Icons.Spinner className="size-6 animate-spin" />
+              <Icons.Spinner className='size-6 animate-spin' />
               <span>Application is loading...</span>
             </div>
           </ClerkLoading>
 
-          <ClerkLoaded>
-            {children}
-          </ClerkLoaded>
+          <ClerkLoaded>{children}</ClerkLoaded>
+          <Toaster />
         </body>
       </html>
-    </ClerkProvider>
+    </Providers>
   );
 }
