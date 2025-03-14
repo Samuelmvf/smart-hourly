deb4fb1704fd9a77db88f6c438dba71aaa6784b2
Samuel Maia <samuelmvf@gmail.com>
2025-03-14 17:25:12 -0300

    feat: add utility function for date conversion

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -11,11 +11,7 @@ interface MoneyFormatterParams {
   value: string | number;
 }
 
-export const numberToCurrencyAdapter = ({
-  language,
-  currency,
-  value,
-}: MoneyFormatterParams) =>
+export const numberToCurrencyAdapter = ({ language, currency, value }: MoneyFormatterParams) =>
   Intl.NumberFormat(language, {
     currency: currency,
     currencyDisplay: "symbol",
@@ -25,5 +21,7 @@ export const numberToCurrencyAdapter = ({
     maximumFractionDigits: 2,
   }).format(Number(value));
 
-export const numberToBRL = (value: string | number) =>
-  numberToCurrencyAdapter({ language: "pt-BR", currency: "BRL", value });
+export const numberToBRL = (value: string | number) => numberToCurrencyAdapter({ language: "pt-BR", currency: "BRL", value });
+
+export const dateToLocaleStringDateAndTime = (date: string, language: string = "en-US") =>
+  new Date(date).toLocaleString(language, { dateStyle: "short", timeStyle: "short" });
